94f615b4b14dcf1ae2bde64adcb1c48e32d58cf4
profs <profs@DIONYSUS>
2024-07-04 09:42:47 +0100

    Add project files.

---
+++ CalculateWordScoreTask/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CalculateWordScoreTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Enter a word or phrase to calculate the score: ");
+            input = Console.ReadLine ();
+            string onlyLetters = removeNonLetters(input);
+
+            // Your task is to calculate the score of the word or phrase
+            // convert each letter to its ascii value and sum them up
+            // print the score
+            // Write your code here
+            //
+        }
+
+        // This method removes all non-letter characters from the input string
+        static string removeNonLetters(string input)
+        {
+            string result = "";
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    result += c;
+                }
+            }
+            return result.ToUpper();
+        }
+    }
+}
